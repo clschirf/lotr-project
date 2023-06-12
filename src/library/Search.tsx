9e489023b4fbd289d3ce87b3b1eb46d3ec5a6802
clschirf <clschirf@gmail.com>
2023-06-12 01:09:22 -0500

    making the search bar cuter

--- src/library/Search.tsx
+++ src/library/Search.tsx
@@ -1,8 +1,12 @@
-import { SyntheticEvent } from 'react';
+import { SyntheticEvent, useState } from 'react';
+import styled from '@emotion/styled';
+import SearchIcon from 'assets/search-solid.svg';
+
 // import debounce from 'lodash/debounce';
 
 export type SearchProps = {
   handleOnChange: (event: SyntheticEvent) => void;
+  placeholder?: string;
 };
 
 /*
@@ -16,17 +20,88 @@ event isnt getting passed down:
     }}
 */
 
+const SearchBarContainer = styled.div`
+  border: 1px solid #9c9c9c;
+  border-radius: 1em;
+  width: 30vw;
+  max-width: 30vw;
+  display: flex;
+  flex-direction: row;
+  height: 2em;
+  align-items: center;
+  padding: 0.25em;
+  cursor: text;
+
+  &.focussed {
+    border-color: #52fa7a;
+  }
+`;
+
+const SearchBar = styled.input`
+  border: none;
+  width: 100%;
+
+  &::placeholder {
+    font-style: italic;
+  }
+
+  &:focus {
+    outline: 0;
+    cursor: text;
+  }
+
+  &:active,
+  &:focus {
+    outline: 0;
+  }
+
+  & .searchie {
+    fill: red;
+  }
+`;
+
+const IconWrapper = styled.div`
+  height: 1.25em;
+  padding: 0.25em;
+  transform: scaleX(-1);
+
+  &.focussed {
+    color: #52fa7a;
+    fill: #52fa7a;
+  }
+`;
+
 const Search = (props: SearchProps) => {
+  const { handleOnChange, placeholder } = props;
+  const [focussed, setFocussed] = useState(false);
+
+  const toggleFocus = () => {
+    setFocussed(isFocussed => !isFocussed);
+  };
+
+  const onKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.keyCode === 27) {
+      setFocussed(false);
+    }
+  };
+
   return (
-    <form>
-      <label htmlFor="searchInput" />
+    <SearchBarContainer className={focussed ? 'focussed' : ''}>
+      <IconWrapper className={focussed ? 'focussed' : ''}>
+        <SearchIcon height="100%" />
+      </IconWrapper>
       {/* TODO: add debounce */}
-      <input
+      <SearchBar
         id="searchInput"
-        placeholder="Find a character"
-        onChange={props.handleOnChange}
+        type="text"
+        maxLength={500}
+        placeholder={placeholder ? placeholder : "Find a character"}
+        onChange={handleOnChange}
+        onFocus={toggleFocus}
+        onBlur={toggleFocus}
+        onKeyUp={onKeyUp}
       />
-    </form>
+    </SearchBarContainer>
   );
 };
 
