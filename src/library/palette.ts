3a4b2d8fa9b07b17ac80ea6ed449c1fb557db628
clschirf <clschirf@gmail.com>
2023-06-13 21:37:13 -0500

    Adding palette and extra icons

---
+++ src/library/palette.ts
@@ -0,0 +1,16 @@
+export const bodyColor = '#232624';
+export const subheadingColor = '#9c9c9c';
+export const primaryColor = '#52fa7a';
+
+export const genderBadge = '#FFBF7C';
+
+export const elf = '#109263';
+export const dwarf = '#CF2C0E';
+export const human = '#FFB974';
+export const ent = '#68A816';
+export const maiar = '#A335DC';
+export const ainur = '#3381ff';
+export const orc = '#B67134';
+export const spiders = '#2B2B2B';
+export const dragons = '#D4AF37';
+export const wraith = '#717171';
